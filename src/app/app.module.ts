f2f3e8307f89cc931939398301832af4430badc5
gvorn <157733779+gvorn7@users.noreply.github.com>
2024-04-23 22:39:17 +0700

    v8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { SideNavComponent } from './side-nav/side-nav.component';
-import { MainComponent } from './main/main.component';
-import { TopWidgetsComponent } from './top-widgets/top-widgets.component';
-import { LastFewTransactionsComponent } from './last-few-transactions/last-few-transactions.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { FooterComponent } from './footer/footer.component';
-import { RequestComponent } from './request/request.component';
-import { ListRequestComponent } from './list-request/list-request.component';
-import { ConfirmComponent } from './confirm/confirm.component';
-import { CalculateComponent } from './calculate/calculate.component';
-import { AdminComponent } from './admin/admin.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { HomepageComponent } from './homepage/homepage.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ToastrModule } from 'ngx-toastr';
-import { MaterialModule } from './material.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableModule } from '@angular/material/table';
-import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ToastrModule } from 'ngx-toastr';
+import { AdminComponent } from './admin/admin.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CalculateComponent } from './calculate/calculate.component';
+import { ConfirmComponent } from './confirm/confirm.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HistoryComponent } from './history/history.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LastFewTransactionsComponent } from './last-few-transactions/last-few-transactions.component';
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
+import { MaterialModule } from './material.module';
+import { RegisterComponent } from './register/register.component';
+import { RequestComponent } from './request/request.component';
 import { ReturnComponent } from './return/return.component';
-import { HistoryComponent } from './history/history.component'
+import { SideNavComponent } from './side-nav/side-nav.component';
 
 
 @NgModule({
@@ -39,7 +37,6 @@ import { HistoryComponent } from './history/history.component'
     HeaderComponent,
     SideNavComponent,
     MainComponent,
-    TopWidgetsComponent,
     LastFewTransactionsComponent,
     FooterComponent,
     RequestComponent,
